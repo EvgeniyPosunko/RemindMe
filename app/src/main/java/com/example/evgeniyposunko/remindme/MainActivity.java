f3856813f48ba369e94c246db54708f05e8b6b94
EvgeniyPosunko <evgeniyposunko@gmail.com>
2018-11-21 13:33:57 +0200

    [RM-7] Создал NavigationHeader и Item(напоминание)

--- app/src/main/java/com/example/evgeniyposunko/remindme/MainActivity.java
+++ app/src/main/java/com/example/evgeniyposunko/remindme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.evgeniyposunko.remindme;
 
 import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,16 +9,22 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity { //AppCompatActivity-поддержка всего с дизайнЛайбрери
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault); //указал стиль темы дефолт
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(LAYOUT);
+    
         initToolbar();
+        ititNavigationView();
     }
+
 
     private void initToolbar() {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
@@ -30,4 +37,9 @@ public class MainActivity extends AppCompatActivity { //AppCompatActivity-под
         });
         toolbar.inflateMenu(R.menu.menu);
     }
+
+    private void ititNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
 }
