114b0f10159d505cbda2569dbcaccbbd3e3b62ad
EvgeniyPosunko <evgeniyposunko@gmail.com>
2018-11-23 15:17:19 +0200

    [RM-11] Добавил четыре фрагмента

--- app/src/main/java/com/example/evgeniyposunko/remindme/MainActivity.java
+++ app/src/main/java/com/example/evgeniyposunko/remindme/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.evgeniyposunko.remindme;
 
-import android.app.Activity;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -8,12 +8,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
 
-import com.example.evgeniyposunko.remindme.adapter.TabsPagerFragmentAdapter;
+import android.view.MenuItem;
+
+import com.example.evgeniyposunko.remindme.adapter.TabsFragmentAdapter;
 import com.example.evgeniyposunko.remindme.fragment.Constants;
 
 public class MainActivity extends AppCompatActivity { //AppCompatActivity-поддержка всего с дизайнЛайбрери
@@ -55,7 +54,7 @@ public class MainActivity extends AppCompatActivity { //AppCompatActivity-под
 
     private void initTabs() {
         viewPager = (ViewPager)findViewById(R.id.viewPager);
-        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        TabsFragmentAdapter adapter = new TabsFragmentAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
